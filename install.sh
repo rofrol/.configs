c6224339f850a8e92d7ad5f07af990837e3b92cd
Roman Frołow <rofrol@gmail.com>
2013-10-11 12:56:29 +0100

    install script creating links

---
+++ install.sh
@@ -0,0 +1,6 @@
+[ -d ~/.config ] || mkdir ~/.config
+ln -sf ~/.configs/.vimrc ~
+ln -sf ~/.configs/.gitconfig ~
+ln -sf ~/.configs/.bash_profile ~
+ln -sf ~/.configs/.elinks ~
+ln -sf ~/.configs/flake8 ~/.config/
