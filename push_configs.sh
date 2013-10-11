9689fe8ae94d0349cd99c5ccb83e39cb88918be3
Roman Frołow <rofrol@gmail.com>
2013-10-11 18:23:59 +0100

    finally works with rsync

--- push_configs.sh
+++ push_configs.sh
@@ -2,10 +2,16 @@
 # 1. ssh-keygen -t rsa (enter, enter ...)
 # 2. ssh-copy-id ${user}@${ip}
 # 3. ./<script_name> ip user1 user2
+# http://serverfault.com/questions/12102/how-to-perform-a-secure-rsync-between-servers-across-an-unsecured-network
+# http://stackoverflow.com/questions/15687755/how-to-use-rsync-to-copy-only-specific-subdirectories-same-names-in-several-dir
 ip=$1
 shift
 for user in "$@"; do
-	scp -r ~/.configs ${user}@${ip}:~
-	scp -r ~/.vim ${user}@${ip}:~
-	ssh ${user}@${ip} 'sh ~/.configs/install.sh'
+	# scp was wrongly copying contents of directory instead whole dir
+	# http://unix.stackexchange.com/a/42691/37431
+	# I think rsync's filter rules can't match the toplevel directory, so it's always synchronized.
+	# A workaround is to synchronize all the files inside this directory instead of the directory itself.
+	# Add .[!.]* after * if you have dot files in the topmost directory,
+	# and ..?* if you have file names beginning with two dots.
+	rsync -azv --include-from 'include.txt' --exclude '*' ~/.[!.]* ${user}@${ip}:
 done
