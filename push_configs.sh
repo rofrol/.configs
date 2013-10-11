3f9d24105be5b4456ec01867a88ea3cb625e8157
Roman Frołow <rofrol@gmail.com>
2013-10-11 14:52:46 +0100

    install.sh for links, push_configs.sh for copying to remote

---
+++ push_configs.sh
@@ -0,0 +1,10 @@
+# 1. ssh-keygen -t rsa (enter, enter ...)
+# 2. ssh-copy-id ${user}@${ip}
+# 3. ./<script_name> ip user1 user2
+ip=$1
+shift
+for user in "$@"; do
+	scp -r ~/.configs ${user}@${ip}:~
+	scp -r ~/.vim ${user}@${ip}:~
+	ssh ${user}@${ip} 'sh ~/.configs/install.sh'
+done
