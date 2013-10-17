bc2b1f92a6ee730ed5073aa21b5e4d2a842b1549
Roman Frołow <rofrol@gmail.com>
2013-10-17 17:53:43 +0100

    downloading vundle

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.config
+[[ -d ~/.vim/bundle/vundle ]] || git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -sf ~/.configs/.vimrc ~
 ln -sf ~/.configs/.gitconfig ~
 ln -sf ~/.configs/.bash_profile ~
